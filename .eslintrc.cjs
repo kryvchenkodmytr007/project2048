026b862ae271cedc4369b4b45868ed3abaad21dd
kryvchenkodmytr007 <kryvchenkodmytr007@gmail.com>
2025-06-11 16:24:24 +0300

    Project config and setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-console': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+};
---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
+  trailingComma: 'all',
+  tabWidth: 2,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+};
