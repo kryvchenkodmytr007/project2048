aecba7d02b180ad729be0e4680b1d8afdae263b1
kryvchenkodmytr007 <kryvchenkodmytr007@gmail.com>
2025-06-17 20:36:58 +0300

    fix: base url added for different scenarios

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
+const repo = 'project2048';
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? `/${repo}/` : '/',
   plugins: [react(), tailwindcss()],
 });
